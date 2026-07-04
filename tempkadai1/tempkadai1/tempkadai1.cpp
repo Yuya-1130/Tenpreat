bd2b6424f4c2666f216010a3eb94dd922723d49a
Yuya-1130 <ktc25a31a0059@edu.kyoto-tech.ac.jp>
2026-07-04 12:40:46 +0900

    途中

---
+++ tempkadai1/tempkadai1/tempkadai1.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+
+
+template<class T>
+class Data {
+public:
+    T Addition(T value, T value2) {
+        T res = value + value2;
+        std::cout << res << std::endl;
+    }
+    void subtraction() {
+
+    }
+    void multiplication() {
+
+    }
+    void division() {
+
+    }
+};
+
+int main()
+{
+  
+}
+
